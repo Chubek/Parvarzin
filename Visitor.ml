cfec65fdd8a3549cf47b3eae6364b25a739f319b
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-08 01:08:20 +0330

    formatted: parser.ml and visitor.ml

--- Visitor.ml
+++ Visitor.ml
@@ -3,7 +3,8 @@ type 'a genealogy_visitor = {
   visit_person_statement : string -> 'a attribute list -> 'a;
   visit_relationship_statement : string -> 'a relationship_value list -> 'a;
   visit_syntactic_comment : string -> 'a;
-  visit_relationship_value : 'a relationship_modifier option -> relationship_type -> string -> 'a;
+  visit_relationship_value :
+    'a relationship_modifier option -> relationship_type -> string -> 'a;
   visit_attribute : string -> 'a attribute_value -> 'a;
 }
 
@@ -16,19 +17,30 @@ and 'a attribute_value =
   | Month of int
   | Year of int
 
-and relationship_modifier =
-  | Maternal | Paternal | Male | Female
+and relationship_modifier = Maternal | Paternal | Male | Female
 
 and relationship_type =
-  | Parent | Child | Spouse | Sibling | Grandparent | Grandchild
-  | Aunt | Uncle | Cousin | Niece | Nephew | InLaw | HalfSibling
-  | StepParent | StepChild
+  | Parent
+  | Child
+  | Spouse
+  | Sibling
+  | Grandparent
+  | Grandchild
+  | Aunt
+  | Uncle
+  | Cousin
+  | Niece
+  | Nephew
+  | InLaw
+  | HalfSibling
+  | StepParent
+  | StepChild
 
 and 'a relationship_value =
-  | RelationshipValue of relationship_modifier option * relationship_type * string
+  | RelationshipValue of
+      relationship_modifier option * relationship_type * string
 
-and 'a attribute =
-  | Attribute of string * 'a attribute_value
+and 'a attribute = Attribute of string * 'a attribute_value
 
 and 'a genealogy_dsl =
   | Parvarzin of string * 'a list
@@ -38,14 +50,24 @@ and 'a genealogy_dsl =
 
 let rec accept_genealogy_visitor visitor ast =
   match ast with
-  | Parvarzin (id, statements) -> visitor.visit_parvarzin id (List.map (accept_genealogy_visitor visitor) statements)
-  | PersonStatement (id, attributes) -> visitor.visit_person_statement id (List.map (accept_genealogy_visitor_attribute visitor) attributes)
-  | RelationshipStatement (id, relationships) -> visitor.visit_relationship_statement id (List.map (accept_genealogy_visitor_relationship_value visitor) relationships)
+  | Parvarzin (id, statements) ->
+      visitor.visit_parvarzin id
+        (List.map (accept_genealogy_visitor visitor) statements)
+  | PersonStatement (id, attributes) ->
+      visitor.visit_person_statement id
+        (List.map (accept_genealogy_visitor_attribute visitor) attributes)
+  | RelationshipStatement (id, relationships) ->
+      visitor.visit_relationship_statement id
+        (List.map
+           (accept_genealogy_visitor_relationship_value visitor)
+           relationships)
   | SyntacticComment comment -> visitor.visit_syntactic_comment comment
 
 and accept_genealogy_visitor_attribute visitor attribute =
   match attribute with
-  | Attribute (name, value) -> visitor.visit_attribute name (accept_genealogy_visitor_attribute_value visitor value)
+  | Attribute (name, value) ->
+      visitor.visit_attribute name
+        (accept_genealogy_visitor_attribute_value visitor value)
 
 and accept_genealogy_visitor_attribute_value visitor attribute_value =
   match attribute_value with
@@ -59,5 +81,5 @@ and accept_genealogy_visitor_attribute_value visitor attribute_value =
 
 and accept_genealogy_visitor_relationship_value visitor relationship_value =
   match relationship_value with
-  | RelationshipValue (modifier, relType, id) -> visitor.visit_relationship_value modifier relType id
-
+  | RelationshipValue (modifier, relType, id) ->
+      visitor.visit_relationship_value modifier relType id
